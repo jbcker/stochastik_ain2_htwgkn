d91a571031251fa8452c9570606c4fc77f816dab
jbcker <jona.boecker@web.de>
2022-05-12 12:47:15 +0200

    Klausur ausgabeverbesserung

--- 3.1.5.py
+++ 3.1.5.py
@@ -1,3 +1,5 @@
+import statistics
+
 import numpy as np
 
 wohnung = [4, 3, 5, 1, 5, 2, 1, 3, 1, 3]
@@ -13,6 +15,7 @@ print("b) Median = ", np.quantile(wohnung, 0.5))
 
 modalarray = np.unique(wohnung, return_counts=True)
 print("c) Modalwert(e): ", np.unique(wohnung, return_counts=True))
+print("c) Modalwert(e): ", statistics.multimode(wohnung))
 # am häufigsten auftretender Wert -> jewl. erster der Arrays
 
 print("d) 25% Quantil = ", np.quantile(wohnung, 0.25))
--- Klausur_Statistik.py
+++ Klausur_Statistik.py
@@ -7,6 +7,7 @@ from numpy import double
 
 
 def ausgabe(dataAsString):
+    ergebnis = ''
     data = [0.0] * (len(dataAsString))
     for x in range(0, len(dataAsString)):
         try:
@@ -16,36 +17,36 @@ def ausgabe(dataAsString):
                 title='Du Seggl',
                 message='Deine Eingabe ist keine Zahl')
 
-    print("Als Float-Array: ", data)
+    ergebnis = ergebnis + "Als Float-Array: " + str(data)
     n = len(data)
-    print("Anzahl Elemente (n): ", n, "\n")
+    ergebnis = ergebnis + "\nAnzahl Elemente (n): " + str(n)
 
-    print("Arithmetisches Mittel = ", np.mean(data))
+    ergebnis = ergebnis + "\nArithmetisches Mittel = " + str(np.mean(data))
     # numpy.mean() berechnet Arithmetisches Mittel(Mittelwert)
-    print("Durchschnitt = ", np.average(data))
+
+    ergebnis = ergebnis + "\nDurchschnitt = " + str(np.average(data))
     # alternative zu np.mean()
 
     print("Median = ", np.quantile(data, 0.5))
     # percentile(array, prozentzahl)berechnet Quantil bei Prozentzahl. Median = 0.5
 
-    # modalarray = np.unique(data, return_counts=True)
-    print("Modalwert(e): ", np.unique(data, return_counts=True))
+    ergebnis = ergebnis + "\nModalwert(e): " + str(statistics.multimode(data))
     # am häufigsten auftretender Wert -> jewl. erster der Arrays
 
-    print("Varianz = ", np.var(data))
+    ergebnis = ergebnis + "\nVarianz = " + str(np.var(data))
     # variance = (Summe der Quadrate aus allen Zahlen und Mitteln)/n
 
-    print("empirische Standardabweichung =", np.std(data))
+    ergebnis = ergebnis + "\nempirische Standardabweichung =" + str(np.std(data))
     # StandardDeviation = Wurzel(variance) -> wie Weit weichen Daten von Mittelwert ab?
 
-    print("Interquartialabstand: ", np.percentile(data, 75) - np.percentile(data, 25))
+    ergebnis = ergebnis + "\nInterquartialabstand: " + str(np.percentile(data, 75) - np.percentile(data, 25))
 
-    print("Spannweite (Range) = ", max(data) - min(data))
+    ergebnis = ergebnis + "\nSpannweite (Range) = " + str(max(data) - min(data))
 
-    print("")
+    loesung(ergebnis)
 
 
-def quantile():
+def quantile(event=None):
     if e.get() != '':
         dataAsString = e.get().split()
         data = [0.0] * (len(dataAsString))
@@ -57,9 +58,13 @@ def quantile():
                     title='Du Käpsale',
                     message='Deine Eingabe ist keine Zahl')
 
-        print("25% Quartil = ", np.quantile(data, 0.25, method="averaged_inverted_cdf"))
-        print("50% Quartil = ", np.quantile(data, 0.50, method="averaged_inverted_cdf"))
-        print("75% Quartil = ", np.quantile(data, 0.75, method="averaged_inverted_cdf"))
+        quantiletxt = ''
+        # print("25% Quartil = ", np.quantile(data, 0.25, method="averaged_inverted_cdf"))
+        quantiletxt = quantiletxt + "25.0% Quartil = " + str(np.quantile(data, 0.25, method="averaged_inverted_cdf"))
+        # print("50% Quartil = ", np.quantile(data, 0.50, method="averaged_inverted_cdf"))
+        quantiletxt = quantiletxt + "\n50.0% Quartil = " + str(np.quantile(data, 0.50, method="averaged_inverted_cdf"))
+        # print("75% Quartil = ", np.quantile(data, 0.75, method="averaged_inverted_cdf"))
+        quantiletxt = quantiletxt + "\n75.0% Quartil = " + str(np.quantile(data, 0.75, method="averaged_inverted_cdf"))
 
         if i.get() != '':
             quantilvalue = i.get()
@@ -72,11 +77,35 @@ def quantile():
             else:
                 print(quantilvalue, "% Quantil = ",
                       np.quantile(data, quantilvalue / 100, method="averaged_inverted_cdf"))
+                quantiletxt = quantiletxt + "\n" + str(quantilvalue) + "% Quantil = " + str(
+                    np.quantile(data, quantilvalue / 100, method="averaged_inverted_cdf"))
     else:
         print("\nKeine Daten eingegeben!\n")
 
+    qnt = tk.Tk()
+    qnt.geometry("500x100")
+    qnt.wm_title("Quantile und so")
+    #frm_qnt = ttk.Frame(qnt, padding=25)
+    #frm_qnt.grid()
 
-def callback():
+    text = tk.Text(qnt, height=5, width=50)
+    scroll = ttk.Scrollbar(qnt)
+    text.configure(yscrollcommand=scroll.set)
+    text.pack(side=tk.LEFT)
+
+    scroll.config(command=text.yview)
+    scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+    text.insert(tk.END, quantiletxt)
+    text['state'] = 'disabled'
+    #text.grid(column=0, row=0)
+
+    b1 = Button(qnt, text="Exit", command=qnt.destroy)
+    #b1.grid(column=1, row=0)
+    b1.pack()
+
+
+def callback(event=None):
     # print (e.get()) # This is the text you may want to use later
     if e.get() != '':
         data = e.get().split()
@@ -86,7 +115,32 @@ def callback():
         print("\nKeine Daten eingegeben!\n")
 
 
-root = Tk()
+def loesung(data):
+    lsg = tk.Tk()
+    lsg.geometry("500x200")
+    lsg.wm_title("Des han i schnell im Kopf gmacht!")
+
+    text = tk.Text(lsg, height=10, width=50)
+    scroll = ttk.Scrollbar(lsg)
+    text.configure(yscrollcommand=scroll.set)
+    text.pack(side=tk.LEFT)
+
+    scroll.config(command=text.yview)
+    scroll.pack(side=tk.RIGHT, fill=tk.Y)
+    n = len(data)
+
+    text.insert(tk.END, data)
+    text['state'] = 'disabled'
+
+    b1 = Button(lsg, text="Exit", command=lsg.destroy)
+    b1.pack()
+
+
+def close(event):
+    sys.exit()  # if you want to exit the entire thing
+
+
+root = tk.Tk()
 root.wm_title("Kannsch des nich im Kopf rechna?!")
 frm = ttk.Frame(root, padding=25)
 frm.grid()
@@ -106,4 +160,7 @@ ttk.Button(frm, text="Drück mi", command=lambda: showinfo(
         title='HaHa',
         message='Hast du während deiner Klausur nichts besseres zu tun?')).grid(column=0, row=5)
 ttk.Button(frm, text="Exit", command=root.destroy).grid(column=1, row=5)
+e.bind('<Return>', callback)
+i.bind('<Return>', quantile)
+root.bind('<Escape>', close)
 root.mainloop()
