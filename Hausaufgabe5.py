ad103df34f09ae16d908e6c02c97d234222b3b6a
DerLux <lukas.butscher@gmx.de>
2022-05-16 09:47:17 +0200

    Aufgabe 3.9

---
+++ Hausaufgabe5.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+print("Autoren: Jona Böcker, Lukas Butscher, Wiebke Prinz, Tobias Stöhr")
+
+print("\nAufgabe 3.6:\n")
+
+print("\nAufgabe 3.9:\n")
+
+print("Baumdiagramm")
+im1 = Image.open("3.9.png")
+im1.show()
+
+print("\nAufgabe 3.11:\n")
+
+print("\nAufgabe 4.1:\n")
+
+print("\nAufgabe 4.2:\n")
+
+print("\nAufgabe 4.3:\n")
+
+
+
