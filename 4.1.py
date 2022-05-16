dfffc7933b202b5908b75d4ddfb5b66a644fb6b5
jbcker <jona.boecker@web.de>
2022-05-16 19:43:32 +0200

    Hausaufgabe 5

---
+++ 4.1.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("4.1.1")
+
+x = [1, 2, 3]
+px1 = [round(4/8, 4), round(1/8, 4), round(3/8, 4)]
+px2 = [round(3/8, 4), round(3/8, 4), round(2/8, 4)]
+fx1 = [px1[0], px1[0] + px1[1], px1[0] + px1[1] + px1[2]]
+fx2 = [px2[0], px2[0] + px2[1], px2[0] + px2[1] + px2[2]]
+
+print("a)")
+print("x1\t\t", "1\t\t", "2\t\t", "3\t\t")
+print("P(X=x1)\t", round(4/8, 4), "\t", round(1/8, 4), "\t", round(3/8, 4))
+print("")
+print("x2\t\t", "1\t\t", "2\t\t", "3\t\t")
+print("P(X=x1)\t", round(3/8, 4), "\t", round(3/8, 4), "\t", round(2/8, 4))
+print("\n")
+print("x\t\t", "1\t\t", "2\t\t", "3\t\t")
+print("F(x1)\t", fx1[0], "\t", fx1[1], "\t", fx1[2], "\t")
+print("")
+print("x\t\t", "1\t\t", "2\t\t", "3\t\t")
+print("F(x2)\t", fx2[0], "\t", fx2[1], "\t", fx2[2], "\t")
+
+
+print("4.1.1 b)")
+
+# set width of bar
+barWidth = 0.25
+fig = plt.subplots()
+x2 = [x + barWidth for x in x]
+
+plt.bar(x, px1, color ='r', width = barWidth,
+        edgecolor ='grey', label ='P(x=x1)')
+plt.bar(x2, px2, color ='g', width = barWidth,
+        edgecolor ='grey', label ='P(x=x2)')
+# Adding Xticks
+plt.ylim(0, 1)
+plt.xlim(0.5, 3.5)
+plt.xlabel('x', fontweight ='bold', fontsize = 15)
+plt.ylabel('P(X=x)', fontweight ='bold', fontsize = 15)
+plt.xticks([r + barWidth for r in range(4)],
+           ['0', '1', '2', '3'])
+plt.title("4.1.1 b)")
+
+plt.legend()
+plt.show()
+
+print("4.1.1 c)")
+typ1 = [1, 1, 1, 1, 2, 3, 3, 3]
+typ2 = [1, 1, 1, 2, 2, 2, 3, 3]
+
+b = plt.figure(2)
+plt.hist(typ1, bins=9, density=True, cumulative=True, label='CDF', histtype='step')
+plt.hist(typ2, bins=9, density=True, cumulative=True, label='CDF', histtype='step')
+plt.xlabel("X")
+plt.ylabel("F(x)")
+plt.xticks(np.arange(1, 4))
+plt.title("4.1.1 c)")
+b.show()
+
+print("4.1.2")
+print("b) Varianz x1 = ", np.var(typ1))
+print("b) Varianz x2 = ", np.var(typ2))
+
+print("h) Standardabweichung x1=", np.std(typ1))
+print("h) Standardabweichung x2=", np.std(typ1))
--- Hausaufgabe5.py
+++ Hausaufgabe5.py
@@ -2,21 +2,36 @@ from PIL import Image
 
 print("Autoren: Jona Böcker, Lukas Butscher, Wiebke Prinz, Tobias Stöhr")
 
-print("\nAufgabe 3.6:\n")
+print("3.6, 3.9, 3.11 als Foto hochgeladen")
 
-print("\nAufgabe 3.9:\n")
+print("\nAufgabe 4.3.1:\n")
 
-print("Baumdiagramm")
-im1 = Image.open("3.9.png")
-im1.show()
+print("a) {(1),(2,1),(3,1),...,(5,6,6,6,6),(6,6,6,6,6)}\n")
 
-print("\nAufgabe 3.11:\n")
+print("b) {1,2,3,4,5}\n")
 
-print("\nAufgabe 4.1:\n")
 
-print("\nAufgabe 4.2:\n")
+print("\nAufgabe 4.3.2:\n")
 
-print("\nAufgabe 4.3:\n")
+print("a)")
+print("x\t\t", "1\t\t\t", "2\t\t\t", "3\t\t\t", "4\t\t\t", "5\t\t\t")
+print("P(X=x)\t", round(1/6, 4), "\t", round(5/6 * 1/6, 4), "\t", round(5/6 * 5/6 * 1/6, 4), "\t", round(5/6 * 5/6 * 5/6 * 1/6, 4), "\t", round(5/6 * 5/6 * 5/6 * 5/6, 4))
+
+print("\nb) ")
+print("x\t\t", "1\t\t\t", "2\t\t\t", "3\t\t\t", "4\t\t\t", "5\t\t\t")
+print("F(x)\t", round(1/6, 4), "\t", round(1/6 + 5/6 * 1/6, 4), "\t", round(1/6 + 5/6 * 1/6 + 5/6 * 5/6 * 1/6, 4), "\t", round(1/6 + 5/6 * 1/6 + 5/6 * 5/6 * 1/6 + 5/6 * 5/6 * 5/6 * 1/6, 4), "\t", round(1/6 + 5/6 * 1/6 + 5/6 * 5/6 * 1/6 + 5/6 * 5/6 * 5/6 * 1/6 + 5/6 * 5/6 * 5/6 * 5/6, 4))
+
+
+print("\nAufgabe 4.3.3:\n")
+
+print("a) ", round(5/6 * 1/6, 4))
+
+print("b) ", 1)
+
+print("c) ", round(1/6 + 5/6 * 1/6 + 5/6 * 5/6 * 1/6, 4))
+
+print("d) ", round(1 - (1/6 + 5/6 * 1/6 + 5/6 * 5/6 * 1/6 + 5/6 * 5/6 * 5/6 * 1/6), 4))
+
 
 
 
